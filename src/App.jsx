339a1f53e807e3b53011ac942572583c1b01da70
RITU0509 <hembramritu0509@gmail.com>
2024-12-03 07:54:29 +0530

    ggg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import About from './pages/About';
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-
 import { BrowserRouter, Routes, Route } from "react-router";
 import Contact from './pages/Contact';
 function App() {
@@ -12,13 +11,13 @@ function App() {
       {/* <Home /> */}
       {/* <About/> */}
       <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="/products" element={<Products/>} />
-      <Route path="/contact-us" element={<Contact/>} />
-    </Routes>
-  </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/contact-us" element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
 
     </div>
   )
